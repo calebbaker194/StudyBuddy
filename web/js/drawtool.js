fa53a2920a7edc556500962cb3d36a8a252558f2
Zac Migues <zmigues@leomail.tamuc.edu>
2018-12-11 13:45:00 -0600

    Finished Draw tool

--- web/js/drawtool.js
+++ web/js/drawtool.js
@@ -1,10 +1,9 @@
-	var red = "#ff3300", 
-    clickX = [], 
+	var clickX = [], 
     clickY = [], 
     clickDrag = [],
     canvas = document.getElementById("canvas"),
 	context = canvas.getContext("2d"),
-	color = red, 
+	color = "#ffffff", 
 	radius = 2,
 	paint = false,
 	
@@ -15,7 +14,6 @@
 	},
 	
 	draw = function() {
-		
 		for( i = 0; i <= clickX.length; i++) {
 			context.beginPath();
 			
@@ -25,40 +23,68 @@
 				context.moveTo(clickX[i] - 1, clickY[i]);
 			
 			context.lineTo(clickX[i], clickY[i]);
-			context.strokeStyle = red;
+			
+			context.strokeStyle = color;
 			context.lineCap = "round";
 			context.lineJoin = "round";
 			context.lineWidth = radius;
 			context.stroke();
 		}
-		//context.closePath();
+		context.closePath();
 	},
-		
+	
 	press = function(e) {
-		var mouseX = e.clientX, mouseY = e.clientY;
+		var rect = canvas.getBoundingClientRect(),
+		scaleX = canvas.width / rect.width,
+		scaleY = canvas.height / rect.height,
+		
+	    mouseX = ((e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - rect.left) * scaleX, 
+	    mouseY = ((e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - rect.top) * scaleY;
+		
 		addClick(mouseX, mouseY, false);
 		paint = true;
 		draw();
 	},
-		
+	
 	drag = function(e) {
-		var mouseX = e.clientX, mouseY = e.clientY;
+		var rect = canvas.getBoundingClientRect(),
+		scaleX = canvas.width / rect.width,
+		scaleY = canvas.height / rect.height,
+		
+		mouseX = ((e.changedTouches ? e.changedTouches[0].clientX : e.clientX) - rect.left) * scaleX, 
+	    mouseY = ((e.changedTouches ? e.changedTouches[0].clientY : e.clientY) - rect.top) * scaleY;
+		
 		if(paint) {
 			addClick(mouseX, mouseY, true);
 			draw();
 		}
-	},
 		
-	release = function() {
+		e.preventDefault();
+	},
+	
+	release = function(e) {
 		paint = false;
 		draw();
 	},
-			
-	cancel = function() {
+	
+	cancel = function(e) {
 		paint = false;
 	};
 	
-	canvas.addEventListener("mousedown", press, true);
-	canvas.addEventListener("mousemove", drag, true);
-	canvas.addEventListener("mouseup", release, true);
-	canvas.addEventListener("mouseout", cancel, true);
+	$(".color").click(function() {
+		clickX = [];
+		clickY = [];
+		clickDrag = [];
+		color = $(this).val();
+		if(color === "eraser") {
+			context.beginPath();
+			context.clearRect(0, 0, canvas.width, canvas.height);
+		}
+	});
+	
+	$("#canvas").mousedown(press).mousemove(drag).mouseup(release).mouseout(cancel);
+	
+	canvas.addEventListener("touchstart", press, false);
+	canvas.addEventListener("touchmove", drag, false);
+	canvas.addEventListener("touchend", release, false);
+	canvas.addEventListener("touchcancel", cancel, false);
